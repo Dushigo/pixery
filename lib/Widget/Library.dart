6309c755ab80e5279d38e76eeded50d95da2c69d
Dushigo <anthony.mercadal@epitech.eu>
2020-02-14 14:02:56 +0100

    remove class firebaseService from Library

--- lib/Widget/Library.dart
+++ lib/Widget/Library.dart
@@ -3,28 +3,21 @@ import 'package:flutter/material.dart';
 import 'package:project_flutter/Services/firebase.dart';
 
 class Library extends StatelessWidget {
-  final FirebaseService firebaseService = FirebaseService();
-
   @override
   Widget build(BuildContext context) {
     return GridView.count(
       crossAxisCount: 2,
       children: List<Widget>.generate(100, (int index) {
-        return GestureDetector(
-            onTap: () async {
-              firebaseService.handleSignIn();
-              print('test Insife');
-            },
-            child: Container(
-              decoration: BoxDecoration(border: Border.all()),
-              margin: const EdgeInsets.all(15.0),
-              child: Center(
-                child: Text(
-                  'Item $index',
-                  style: Theme.of(context).textTheme.headline,
-                ),
-              ),
-            ));
+        return Container(
+          decoration: BoxDecoration(border: Border.all()),
+          margin: const EdgeInsets.all(15.0),
+          child: Center(
+            child: Text(
+              'Item $index',
+              style: Theme.of(context).textTheme.headline,
+            ),
+          ),
+        );
       }),
     );
   }
