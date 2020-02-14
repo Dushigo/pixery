43c4e64583951e8f6952f17c4fae0d4e77be1e81
Dushigo <anthony.mercadal@epitech.eu>
2020-02-14 14:16:59 +0100

    remove json

--- lib/Widget/Library.dart
+++ lib/Widget/Library.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:project_flutter/Services/firebase.dart';
 
 class Library extends StatelessWidget {
   @override
